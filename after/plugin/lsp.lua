245b90e9d89bc739d1cf347e34b35efdbf407933
= <=>
2024-11-13 11:38:46 +0700

    chore(lsp): remove duplicate setup and clean up

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,6 +1,8 @@
-local lsp = require("lsp-zero")
+local lsp = require('lsp-zero')
+local lspconfig = require('lspconfig')
+local mason = require("mason")
 
-require('mason').setup({
+mason.setup({
     ui = {
         icons = {
             package_installed = "✓",
@@ -9,53 +11,89 @@ require('mason').setup({
         }
     }
 })
+
+lspconfig.intelephense.setup({})
+lspconfig.lua_ls.setup(lsp.nvim_lua_ls())
+
+lspconfig.svelte.setup {
+    filetypes = { "svelte" },
+    on_attach = function(client, _buffer)
+        vim.api.nvim_create_autocmd("BufWritePost", {
+            pattern = { "*.js", "*.ts", "*.svelte" },
+            callback = function(ctx)
+                client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.file })
+            end,
+        })
+    end
+}
+
+lsp.preset("recommended")
+
+lsp.on_attach(function(_, bufnr)
+    lsp.default_keymaps({ buffer = bufnr })
+    local nmap = function(keys, func, desc)
+        if desc then
+            desc = 'LSP: ' .. desc
+        end
+
+        vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+    end
+
+    nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+    nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+
+    nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+    nmap('<leader>gb', '<C-O>', '[G]o [B]ack')
+    nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+    nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+    nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+    nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+    nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+
+    -- See `:help K` for why this keymap
+    nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+    nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+
+    -- Lesser used LSP functionality
+    nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+    nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+    nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+    nmap('<leader>wl', function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, '[W]orkspace [L]ist Folders')
+    -- Create a command `:Format` local to the LSP buffer
+    vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+        vim.lsp.buf.format()
+    end, { desc = 'Format current buffer with LSP' })
+end)
+
+lsp.setup()
+
+-- here you can setup the language servers
+
 require('mason-lspconfig').setup({
     ensure_installed = {},
     automatic_installation = true,
     handlers = {
         lsp.default_setup,
+        rust_analyzer = function() end
     },
 })
 
-lsp.preset("recommended")
-
 local cmp = require('cmp')
 local luasnip = require 'luasnip'
+local cmp_nvim_lsp = require("cmp_nvim_lsp")
+
 require('luasnip.loaders.from_vscode').lazy_load()
 luasnip.config.setup {}
 
-local lspconfig = require('lspconfig')
-lspconfig.lua_ls.setup(lsp.nvim_lua_ls())
-lspconfig.rust_analyzer.setup {
-    settings = {
-        ['rust-analyzer'] = {
-            check = {
-                command = "clippy",
-            },
-            diagnostics = {
-                enable = true,
-            }
-        }
-    }
-}
-local cmp_nvim_lsp = require("cmp_nvim_lsp")
+
 lspconfig.clangd.setup {
     capabilities = cmp_nvim_lsp.default_capabilities(),
     cmd = {
         "clangd",
         "--offset-encoding=utf-16",
     },
-}
-
-require 'lspconfig'.svelte.setup {
-    on_attach = function(client)
-        vim.api.nvim_create_autocmd("BufWritePost", {
-            pattern = { "*.js", "*.ts" },
-            callback = function(ctx)
-                client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.file })
-            end,
-        })
-    end
 }
 
 cmp.setup {
@@ -108,43 +146,6 @@ lsp.set_preferences({
     }
 })
 
-lsp.on_attach(function(_, bufnr)
-    local nmap = function(keys, func, desc)
-        if desc then
-            desc = 'LSP: ' .. desc
-        end
-
-        vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-    end
-
-    nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-    nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-
-    nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-    nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-    nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-    nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-    nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-    nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-
-    -- See `:help K` for why this keymap
-    nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-    nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
-
-    -- Lesser used LSP functionality
-    nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-    nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-    nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-    nmap('<leader>wl', function()
-        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, '[W]orkspace [L]ist Folders')
-    -- Create a command `:Format` local to the LSP buffer
-    vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-        vim.lsp.buf.format()
-    end, { desc = 'Format current buffer with LSP' })
-end)
-
-lsp.setup()
 
 
 vim.diagnostic.config({
