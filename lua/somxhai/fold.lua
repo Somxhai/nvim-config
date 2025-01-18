7a96d6289c4241b440a690ecad7d910a125cff71
= <ChanakanThongrong@gmail.com>
2025-01-18 15:41:35 +0700

    feat(fold): add fold

---
+++ lua/somxhai/fold.lua
@@ -0,0 +1,7 @@
+vim.o.fillchars = 'eob: ,fold: ,foldopen:,foldsep: ,foldclose:'
+vim.o.foldcolumn = '1'
+vim.o.foldenable = true
+vim.o.foldexpr = 'v:lua.vim.lsp.foldexpr()'
+vim.o.foldlevel = 99
+vim.o.foldlevelstart = 99
+vim.o.foldmethod = 'expr'
