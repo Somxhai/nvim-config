eaea5c5fac5ab04433d2b238fb781e4615968925
= <ChanakanThongrong@gmail.com>
2025-01-18 15:40:06 +0700

    feat(theme): enable theme background

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -1,7 +1,9 @@
 local tokyonight = require("tokyonight")
+
 tokyonight.setup({
     style = "night"
 })
+
 require('nvim-highlight-colors').setup {
     render = "background",
     enable_tailwind = true,
@@ -10,8 +12,8 @@ function ColorMyPencils(color)
     color = color or "tokyonight"
     vim.cmd.colorscheme(color)
 
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 ColorMyPencils()
